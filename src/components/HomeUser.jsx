a6e4ef60129562c22affe95c4d2286dd317e4615
Abraham67 <abrahamcsackie160@gmail.com>
2025-05-15 03:27:46 +0800

    dashboard desg. stage 1

--- src/components/HomeUser.jsx
+++ src/components/HomeUser.jsx
@@ -8,10 +8,10 @@ export const HomeUser = () => {
 
   const nagivate = useNavigate();
   const name = JSON.parse(localStorage.getItem("jwtToken"));
+  if (name == null) {
+    window.location = "login_user";
+  }
   const getname = name.username;
-  const ssID = JSON.stringify(sessionStorage.setItem("userName", getname));
-  const obUser = JSON.parse(sessionStorage.getItem("UserName"));
-  const names = obUser.username;
 
   //tryig the new branch
   return (
@@ -27,7 +27,7 @@ export const HomeUser = () => {
             <path d="M11.25 4.533A9.707 9.707 0 0 0 6 3a9.735 9.735 0 0 0-3.25.555.75.75 0 0 0-.5.707v14.25a.75.75 0 0 0 1 .707A8.237 8.237 0 0 1 6 18.75c1.995 0 3.823.707 5.25 1.886V4.533ZM12.75 20.636A8.214 8.214 0 0 1 18 18.75c.966 0 1.89.166 2.75.47a.75.75 0 0 0 1-.708V4.262a.75.75 0 0 0-.5-.707A9.735 9.735 0 0 0 18 3a9.707 9.707 0 0 0-5.25 1.533v16.103Z" />
           </svg>
           <h1 className="rounded-md text-2xl  text-amber-500 font-extrabold px-3 py-2 cursor-pointer">
-            <Link to="/">AirNote</Link>
+            AirNote
           </h1>
         </div>
         <div className="flex center m-0 text-center relative left-50">
@@ -73,7 +73,7 @@ export const HomeUser = () => {
                   clipRule="evenodd"
                 />
               </svg>
-              <h2 className="text-white">{names}</h2>
+              <h2 className="text-white">{getname}</h2>
             </button>
           </div>
         </div>
--- src/pages/UserWork.jsx
+++ src/pages/UserWork.jsx
@@ -2,23 +2,77 @@ import React, { useEffect, useState } from "react";
 import { HomeUser } from "../components/HomeUser";
 import { notes } from "../utils/axio";
 import { useNavigate } from "react-router-dom";
+// import { DotLottieReact } from "lottie-react/dotlottie-react";
 
 export const UserWork = () => {
   const [note, setNote] = useState([]);
   const nagivate = useNavigate();
+  const user = [
+    {
+      id: 1,
+      name: "John Brown",
+      Age: 45,
+      Location: " Kakata",
+    },
+    {
+      id: 2,
+      name: "Paul John",
+      Age: 44,
+      Location: "Liberia",
+    },
+    {
+      id: 3,
+      name: "John Brown",
+      Age: 45,
+      Location: " Kakata",
+    },
+    {
+      id: 4,
+      name: "Paul John",
+      Age: 44,
+      Location: "Liberia",
+    },
+    {
+      id: 5,
+      name: "John Brown",
+      Age: 45,
+      Location: " Kakata",
+    },
+    {
+      id: 6,
+      name: "Paul John",
+      Age: 44,
+      Location: "Liberia",
+    },
+  ];
 
   useEffect(() => {
-    const userNote = notes();
+    const userNote = user;
+    // notes();
     if (userNote) {
       setNote(userNote);
     }
+
     console.log("Note: " + note);
   }, []);
 
   return (
     <div>
       <HomeUser />
-      UserWork
+      <div>
+        <div className="content">
+          <div className="sidebar"></div>
+          <div>
+            <div className="item">
+              {note.map((data) => (
+                <div key={data.id} className="data">
+                  {data.name}
+                </div>
+              ))}
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
